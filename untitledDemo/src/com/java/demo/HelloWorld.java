6043f3a04a58d8abf06a2efaaa407e0bb6014673
liam <liam@easestrategy.com>
2021-01-19 20:44:46 +0800

    feat:create a helloworld demo

--- untitledDemo/src/com/java/demo/HelloWorld.java
+++ untitledDemo/src/com/java/demo/HelloWorld.java
@@ -6,7 +6,7 @@ package com.java.demo;
  */
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("HelloWorld!");
+        System.out.println("name!");
     }
 
 }
