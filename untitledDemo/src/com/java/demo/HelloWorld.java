9b1b471f16c82ce5fe2a9b2c7c9a50727a406ff5
liam <liam@easestrategy.com>
2021-01-19 20:17:37 +0800

    feat:create a helloworld demo

---
+++ untitledDemo/src/com/java/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.demo;
+
+/**
+ * @Author: liam
+ * @Date: 2021/1/19 20:01
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!");
+    }
+
+}
